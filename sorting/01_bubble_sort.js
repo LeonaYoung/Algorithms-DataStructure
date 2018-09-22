c5ccf4ebe17dc97bd114627fc13f3c026c7ad15b
Leona Yang <leonayang@sf-express.com>
2018-09-22 22:42:39 +0800

    complete bubble sort

--- sorting/01_bubble_sort.js
+++ sorting/01_bubble_sort.js
@@ -3,18 +3,36 @@
  */
 
 const generate_array = (min, max, size) => {
-    return [3, 2, 1]
+    const arr = [];
+    for (let i = 0; i < size; i++) {
+        arr[i] = Math.floor(Math.random() * 200 -100);
+    }
+    return arr;
 }
 
 const validate_order = array => {
-    return false
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 const bubble_sort = array => {
+    for(let j = 0; j < array.length - 1; j++) {
+        for (let i = 0; i < array.length - 1 - j; i++) {
+            const temp = array[i];
+            if (array[i] > array[i + 1]) {
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
     return array
 }
 
-array = generate_array(-100, 100, 3)
+array = generate_array(-100, 100, 10)
 console.log('array: ', array)
 
 array = bubble_sort(array)
