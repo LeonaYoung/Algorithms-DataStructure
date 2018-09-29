50beb3195e21e62fdfd52a5ed0584bdfa90e4205
Leona Yang <leonayang@sf-express.com>
2018-09-29 17:24:38 +0800

    add editorconfig

--- sorting/01_bubble_sort.js
+++ sorting/01_bubble_sort.js
@@ -3,33 +3,33 @@
  */
 
 const generate_array = (min, max, size) => {
-    const arr = [];
-    for (let i = 0; i < size; i++) {
-        arr[i] = Math.floor(Math.random() * (max - min) + min);
-    }
-    return arr;
+  const arr = [];
+  for (let i = 0; i < size; i++) {
+    arr[i] = Math.floor(Math.random() * (max - min) + min);
+  }
+  return arr;
 }
 
 const validate_order = array => {
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            return false;
-        }
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const bubble_sort = array => {
-    for(let j = 0; j < array.length - 1; j++) {
-        for (let i = 0; i < array.length - 1 - j; i++) {
-            const temp = array[i];
-            if (array[i] > array[i + 1]) {
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-            }
-        }
+  for (let j = 0; j < array.length - 1; j++) {
+    for (let i = 0; i < array.length - 1 - j; i++) {
+      const temp = array[i];
+      if (array[i] > array[i + 1]) {
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
     }
-    return array
+  }
+  return array
 }
 
 array = generate_array(-100, 100, 10)
@@ -38,4 +38,4 @@ console.log('array: ', array)
 array = bubble_sort(array)
 
 console.log(validate_order(array) === true ?
-            'It is sorted.' : 'Not sorted')
+  'It is sorted.' : 'Not sorted')
--- sorting/02_insertion_sort.js
+++ sorting/02_insertion_sort.js
@@ -1,53 +1,68 @@
 /*
- * Bubble Sort
+ * insertion Sort
  */
 
 const generate_array = (min, max, size) => {
-    const arr = [];
-    for (let i = 0; i < size; i++) {
-        arr[i] = Math.floor(Math.random() * (max - min) + min);
-    }
-    return arr;
+  const arr = [];
+  for (let i = 0; i < size; i++) {
+    arr[i] = Math.floor(Math.random() * (max - min) + min);
+  }
+  return arr;
 }
 
 const validate_order = array => {
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            return false;
-        }
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const swap = (former, later) => {
-    const temp = later
-    later = former
-    former = temp
-    return former, later
+  const temp = later
+  later = former
+  former = temp
+  return former, later
+}
+
+// question: 直接交换不会改变array[i]的值吗？
+const insertion_sort_0 = array => {
+  for (let i = 1; i < array.length; i++) {
+    let j = i
+    while (j > 0 && array[i] < array[j - 1]) {
+      j--
+      array[j + 1], array[j] = swap(array[j + 1], array[j])
+    }
+  }
+  return array
+}
+
+// 不建议用splice()方法
+const insertion_sort_1 = array => {
+  for (let i = 1; i < array.length; i++) {
+    let j = i
+    while (j > 0 && array[i] < array[j - 1]) {
+      j--
+    }
+    const temp = array[i];
+    array.splice(i, 1);
+    array.splice(j, 0, temp);
+  }
+  return array
 }
 
 const insertion_sort = array => {
-    for (let i = 1; i < array.length; i++) {
-        let j = i
-        while (j > 0 && array[i] < array[j-1]) {
-            j--
-            array[j+1], array[j] = swap(array[j+1], array[j])
-        }
+  for (let i = 1; i < array.length; i++) {
+    const current = array[i];
+    let j = i;
+    while (j > 0 && array[j - 1] > current) {
+      array[j] = array[j - 1];
+      j--;
     }
-    return array
-}
-
-const insertion_sort_0 = array => {
-    for (let i = 1; i < array.length; i++) {
-        let j = i
-        while (j > 0 && array[i] < array[j-1]) {
-            j--
-        }
-        const temp = array[i];
-        array.splice(i, 1);
-        array.splice(j, 0, temp);
-    }
-    return array
+    array[j] = current;
+  }
+  return array;
 }
 
 array = generate_array(-100, 100, 10)
@@ -57,4 +72,4 @@ array = insertion_sort(array)
 console.log('sort array: ', array)
 
 console.log(validate_order(array) === true ?
-            'It is sorted.' : 'Not sorted')
+  'It is sorted.' : 'Not sorted')
