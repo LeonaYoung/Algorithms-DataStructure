a0b82be62b4deb38739c129e9e30a5ffbb28aed8
Leona Yang <leonayang@sf-express.com>
2018-09-29 11:58:48 +0800

    add insertion_sort

--- sorting/02_insertion_sort.js
+++ sorting/02_insertion_sort.js
@@ -19,15 +19,42 @@ const validate_order = array => {
     return true;
 }
 
+const swap = (former, later) => {
+    const temp = later
+    later = former
+    former = temp
+    return former, later
+}
+
 const insertion_sort = array => {
-    // 
+    for (let i = 1; i < array.length; i++) {
+        let j = i
+        while (j > 0 && array[i] < array[j-1]) {
+            j--
+            array[j+1], array[j] = swap(array[j+1], array[j])
+        }
+    }
+    return array
+}
+
+const insertion_sort_0 = array => {
+    for (let i = 1; i < array.length; i++) {
+        let j = i
+        while (j > 0 && array[i] < array[j-1]) {
+            j--
+        }
+        const temp = array[i];
+        array.splice(i, 1);
+        array.splice(j, 0, temp);
+    }
     return array
 }
 
 array = generate_array(-100, 100, 10)
 console.log('array: ', array)
 
-array = validate_order(array)
+array = insertion_sort(array)
+console.log('sort array: ', array)
 
 console.log(validate_order(array) === true ?
             'It is sorted.' : 'Not sorted')
