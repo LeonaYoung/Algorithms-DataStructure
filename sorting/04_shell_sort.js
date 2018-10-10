4ade6f881fc24d3c33c64cce1dfa1e212aa663a9
Leona Yang <leonayang@sf-express.com>
2018-10-10 23:34:11 +0800

    finsh shell sort

--- sorting/04_shell_sort.js
+++ sorting/04_shell_sort.js
@@ -1,6 +1,7 @@
 /*
  * Shell Sort
  *根据待排序数组的长度，算出间隔（增量）；
+ *在数组长度特别大的时候，并且排序特别不理想的情况下有优势
  */
 
 const generate_array = (min, max, size) => {
@@ -20,25 +21,31 @@ const validate_order = array => {
   return true;
 }
 
+
+
 const shell_sort = array => {
   let gap = 1;
   while((3 * gap + 1) < array.length) {
     gap = 3 * gap + 1;
   }
-  console.log('gap', gap);
-  for (let i = gap; i > 0; gap = ((gap - 1) / 3)) {
-    console.log('for', i, gap);
-    // for (let j = 0; j < gap; j++) {
-    //   for (let k = j + gap; k < array.length; k = k + gap) {
-    //     const current = array[k];
-    //     let l = k;
-    //     while(l > 0 && array[l] < array[l - gap]) {
-    //       array[l] = array[l - gap];
-    //       l = l - gap;
-    //     }
-    //     array[l] = current;
-    //   }
-    // }
+  // for (let i = gap; i > 0; gap = ((gap - 1) / 3)) {
+  //   // 死循环了！！！！
+  //   // i的值一直等于gap（等于4），条件永远为真，永远执行；
+  //   // console.log('gap: ', gap)
+  // }
+  while(gap > 0) {
+    for (let j = 0; j < gap; j++) { // for循环中的第一个条件只执行一次啊啊，要不还要j++ 干嘛，那不每次j都等于0了嘛。。
+      for (let k = j + gap; k < array.length; k = k + gap) {
+        const current = array[k];
+        let l = k;
+        while(l > j && current < array[l - gap]) { // 是用当前待插入的项的值和他前面的项去比大小，找到他要插入的位置
+          array[l] = array[l - gap];
+          l = l - gap;
+        }
+        array[l] = current;
+      }
+    }
+    gap = (gap - 1) / 3;
   }
   return array;
 }
@@ -47,7 +54,6 @@ array = generate_array(-100, 100, 10)
 console.log('array: ', array)
 
 array = shell_sort(array)
-console.log('hh', array);
 
 console.log(validate_order(array) === true ?
   'It is sorted.' : 'Not sorted')
